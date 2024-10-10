742242ce3a7f97f3909f31fc197d652a2ce7e073
ruben asua <rubenasuasoto@gmail.com>
2024-10-10 20:42:54 +0200

    commit

---
+++ src/main/kotlin/crud/AppCRUD.kt
@@ -0,0 +1,10 @@
+﻿package crud
+
+import crud.model.Empleado
+import java.nio.file.Path
+
+fun main(){
+
+    val ficheroXML :Path = Path.of("src").resolve("main/resources/empleado.xml")
+    val e:Empleado = Empleado("16","linares","IT","5000")
+}
--- src/main/kotlin/crud/repository/XMLrepositorio.kt
+++ src/main/kotlin/crud/repository/XMLrepositorio.kt
@@ -3,8 +3,15 @@
 import crud.model.Empleado
 import org.w3c.dom.Document
 import org.w3c.dom.Element
+import org.w3c.dom.Text
 import java.nio.file.Path
 import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.Source
+import javax.xml.transform.Transformer
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
 import kotlin.io.path.notExists
 
 class XMLrepositorio {
@@ -23,8 +30,33 @@ class XMLrepositorio {
         val document = parseXML(rutaXml)
         val root: Element = document.documentElement
         root.normalize()
+        // insertar empleado
+        val elementoNuevoEmpleado : Element = document.createElement("empleado")
+        elementoNuevoEmpleado.setAttribute("id",newEmpleado.id)
+        root.appendChild(elementoNuevoEmpleado)
 
+        val apellido: Element = document.createElement("apellido")
+        val departamento: Element = document.createElement("departamento")
+        val salario: Element = document.createElement("salario")
 
+        val textoApellido: Text = document.createTextNode(newEmpleado.apellido)
+        val textoDepartamento: Text = document.createTextNode(newEmpleado.dept)
+        val textoSalario: Text = document.createTextNode(newEmpleado.salario)
+        apellido.appendChild(textoApellido)
+        departamento.appendChild(textoDepartamento)
+        salario.appendChild(textoSalario)
+
+        elementoNuevoEmpleado.appendChild(apellido)
+        elementoNuevoEmpleado.appendChild(departamento)
+        elementoNuevoEmpleado.appendChild(salario)
+        document.documentElement.appendChild(elementoNuevoEmpleado)
+        val source: Source = DOMSource(document)
+        val result = StreamResult(Path.of("src/main/resources/empleado.xml").toFile())
+
+        val transformer: Transformer = TransformerFactory.newInstance().newTransformer()
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes")
+
+        transformer.transform(source, result)
     }
     private fun parseXML(rutaXml: Path): Document {
         val dbf = DocumentBuilderFactory.newInstance()
